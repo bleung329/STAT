8a73ade805b49d17dbcd2086f7f0741387581281
bleung329 <bleung@stuy.edu>
2017-10-31 15:55:21 -0400

    bang bang

--- stat.c
+++ stat.c
@@ -7,8 +7,10 @@ int main()
 {
 	struct stat sb;
 	stat("stat.c", &sb);
-	printf("%d\n", sb.st_atim);
-	printf("%s\n", ctime(&sb.st_atim));
-	return 0;
+	printf("Size of file in bytes: %ld\n", sb.st_size);
+	printf("Permissions: %o\n", sb.st_mode);
+	printf("%ld\n", sb.st_atim);
+	printf("Access time: %s\n", ctime(&sb.st_atim));
+	return 1;
 }
 
