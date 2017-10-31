28d7e8afebf3ffeb5101a39f3b88d4a6e11f8982
ykadir42 <ykadir42@gmail.com>
2017-10-31 17:08:17 -0400

    SomethingSomethingbangbang

--- stat.c
+++ stat.c
@@ -5,12 +5,21 @@
 #include <time.h>
 int main()
 {
-	struct stat sb;
-	stat("stat.c", &sb);
-	printf("Size of file in bytes: %ld\n", sb.st_size);
-	printf("Permissions: %o\n", sb.st_mode);
-	printf("%ld\n", sb.st_atim);
-	printf("Access time: %s\n", ctime(&sb.st_atim));
-	return 1;
+  struct stat sb;
+  stat("WomensRights.pdf", &sb);
+  off_t size = sb.st_size;
+  if(size >= 1073741824)
+    printf("Size of file: %ldGB\n", size/1073741824);
+  else if(size >= 1048576)
+    printf("Size of file: %ldMB\n", size/1048576);
+  else if(size >= 1024)
+    printf("Size of file: %ldKB\n", size/1024);
+  else
+    printf("Size of file: %ldB\n", size);
+  printf("Permissions: %o\n", sb.st_mode);
+  printf("%ld\n", sb.st_atim);
+  printf("Access time: %s\n", ctime(&sb.st_atim));
+
+  return 0;
 }
 
