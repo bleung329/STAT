3903cf8e52b8d645f7c552628a794e1620bf0b44
ykadir42 <ykadir42@gmail.com>
2017-10-31 22:29:51 -0400

    pew pew

--- stat.c
+++ stat.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <sys/stat.h>
-#include <string.h>
-#include <stdlib.h>
 #include <time.h>
-int main()
-{
-  struct stat sb;
-  stat("WomensRights.pdf", &sb);
-  off_t size = sb.st_size;
-  if(size >= 1073741824)
-    printf("Size of file: %ldGB\n", size/1073741824);
-  else if(size >= 1048576)
-    printf("Size of file: %ldMB\n", size/1048576);
-  else if(size >= 1024)
-    printf("Size of file: %ldKB\n", size/1024);
-  else
-    printf("Size of file: %ldB\n", size);
-  printf("Permissions: %o\n", sb.st_mode);
-  printf("%ld\n", sb.st_atim);
-  printf("Access time: %s\n", ctime(&sb.st_atim));
+int main(){
+	struct stat sb;
+	stat("stat.c", &sb);
 
-  return 0;
+	off_t size = sb.st_size;
+	if(size >= 1073741824)
+		printf("Size of file: %ldGB\n", size/1073741824);
+	else if(size >= 1048576)
+		printf("Size of file: %ldMB\n", size/1048576);
+	else if(size >= 1024)
+		printf("Size of file: %ldKB\n", size/1024);
+	else
+		printf("Size of file: %ldB\n", size);
+
+	mode_t mode = sb.st_mode;
+	printf("Permissions: %o\n", mode);
+	char modes[10] = "rwxrwxrwx";
+	int i;
+	for(i = 0; i < 9; i++)
+		if(!(mode & (1 << (8-i))))
+			modes[i] = '-';
+	printf("Permissions: %s\n", modes);
+
+	printf("Access time: %s\n", ctime((time_t *)&sb.st_atim));
+
+	return 0;
 }
-
